ea22a6a07b13b02987883aacb988ad3757cf262a
gurielamp <gurielamp@gmail.com>
2020-03-08 01:10:29 +0700

    add get machine

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
 	$arr_nilai = [];
   $test = "bangkeee";
   
-  $ej_file = file_get_contents('ej.txt');
+  $ej_file = file_get_contents('ej2.txt');
 	$ejString = (str_replace(' ', '', $ej_file));
 
 	function get_string_between($string, $start, $end){
@@ -20,26 +20,22 @@
     return substr($string, $ini, $len);
 	}
 
-	$start = 'TRANSACTIONEND';
-	$end = 'SALDO';
-	$raw_nilai = get_string_between($ejString, $start, $end);
-	$nilai = (str_replace('.', '', $raw_nilai));
+	// $start = 'TRANSACTIONEND';
+	// $end = 'SALDO';
+	// $raw_nilai = get_string_between($ejString, $start, $end);
+	// $nilai = (str_replace('.', '', $raw_nilai));
 
-	$find_tr = strpos($ejString, $start);
-  $get_total_pengisian = 100000000 * 4;
-// echo (str_replace('.', '', get_string_between($ejString, "TRANSACTIONstart", "TRANSACTIONEND")));
-	while ( $find_tr !== false) {
-		$get_nilai = (str_replace('.', '', get_string_between($ejString, $start, $end)));
-		array_push($arr_nilai, $get_nilai);
-		$get_total_nilai += $get_nilai;
-		// echo (str_replace('.', '', get_string_between($ejString, $start, $end)));
-		// echo "<br>";
-		$find_tr = strpos($ejString, $start);
-		$ejString = substr($ejString, $find_tr +8);
-	}
-  $get_sisa_restocking = $get_total_pengisian - $get_total_nilai;
-  // echo "$arr_nilai[2]";
-  echo "<br>";
+	// $find_tr = strpos($ejString, $start);
+ //  $get_total_pengisian = 100000000 * 4;
+	// while ( $find_tr !== false) {
+	// 	$get_nilai = (str_replace('.', '', get_string_between($ejString, $start, $end)));
+	// 	array_push($arr_nilai, $get_nilai);
+	// 	$get_total_nilai += $get_nilai;
+	// 	$find_tr = strpos($ejString, $start);
+	// 	$ejString = substr($ejString, $find_tr +8);
+	// }
+ //  $get_sisa_restocking = $get_total_pengisian - $get_total_nilai;
+ //  echo "<br>";
   
 
 
@@ -50,26 +46,52 @@
   function get_string_between_transaction($string, $start, $end){
 	$ini = strpos($string, $start);
     if ($ini == 0) return '';
-    $ini += strlen($start)-20;
+    $ini += strlen($start);
     $len = strpos($string, $end, $ini) - $ini;
     return substr($string, $ini, $len);
 	}
 
 	// SEPARATING EACH TRANSACTION
 	function get_transaction ($ej_transaction){
-		$find_transaction = strpos($ej_transaction, 'TRANSACTION start');
+		$find_transaction = strpos($ej_transaction, 'CASH REQUEST: ');
 		$arr_transaction = [];
 		while ( $find_transaction !== false) {
-			$get_transaction = get_string_between_transaction($ej_transaction, 'TRANSACTION start', 'TRANSACTION END');
+			$get_transaction = get_string_between_transaction($ej_transaction, 'CASH REQUEST: ', 'CASH TAKEN');
 			array_push($arr_transaction, $get_transaction);
-			$find_transaction = strpos($ej_transaction, 'TRANSACTION start');
-			$ej_transaction = substr($ej_transaction, $find_transaction +8);
+			$find_transaction = strpos($ej_transaction, 'CASH REQUEST: ');
+			$ej_transaction = substr($ej_transaction, $find_transaction +1);
 		} 
 		return $arr_transaction;
 		// 
 	}
 	$transaction = (get_transaction($ej_transaction));
 
+//=======================================================
+
+	$i = 0;
+	while($transaction[$i] !== null){
+		$ns_transaction = (str_replace(' ', '', $transaction[$i]));
+		$get_nilai = get_string_between_transaction($ns_transaction, 'JUMLAHRP' , 'SALDO');
+		$get_noRecord = get_string_between_transaction($ns_transaction, 'NO.REKORD' , 'PENARIKAN');
+		$get_jam = substr($transaction[$i],8,9);
+		array_push($arr_nilai, (str_replace('.', '', $get_nilai)));
+		array_push($arr_noRecord, $get_noRecord);
+		array_push($arr_jam, $get_jam);
+		$i++;
+	}
+$it = 0;
+	while ( $transaction[$it] !== null) {
+		echo $transaction[$it] . '<br> <br>';
+		echo $arr_nilai[$it] . '<br> <br>';
+		echo $arr_noRecord[$it] . '<br> <br>';
+		echo(rand(100000,999999)). '<br> <br>';
+		echo $arr_jam[$it]. '<br> <br>';
+		$it++;
+	}
+
+
+
+//=========================================
 	// function find_cash_transaction(){
 	// 	$cash_transaction = [];
 	// 	$iq = 0;
@@ -80,15 +102,16 @@
 	// 	}return $cash_transaction;
 	// }
 
-		$cash_transaction = [];
-		$iq = 0;
-		while ($transaction[$iq] !== null) {
-			if(strpos($transaction[$iq], "CASH TAKEN") !== false){
-					array_push($cash_transaction, $transaction[$iq]);
-			}$iq++;
-		}
-		// var_dump($cash_transaction);
-		echo $cash_transaction[0] . '<br> <br>' . $cash_transaction[1];
+		// $cash_transaction = [];
+		// $iq = 0;
+		// while ($transaction[$iq] !== null) {
+		// 	if(strpos($transaction[$iq], "CASH PRESENTED") !== false){
+		// 			array_push($cash_transaction, $transaction[$iq]);
+		// 	}$iq++;
+		// }
+	//========================================
+
+		
 
 			// print_r ($a[0]);
 			// echo $b;
